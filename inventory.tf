f1ed51614616623fc0f33746ab17e7a6e7b630a4
RebelWyvern <carolina.fracao@acad.pucrs.br>
2024-07-13 18:12:17 -0300

    arrumando o inventory.tf

--- inventory.tf
+++ inventory.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.0.0"
-    }
-  }
-}
 
 resource "local_file" "inventory" {
   content = templatefile("inventory.ini", {
